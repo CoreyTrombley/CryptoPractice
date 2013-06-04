8ee9361470a7b7fefdbdb1fa6210f6137311451e
Dan Garland <dan@dangarland.co.uk>
2013-06-04 17:32:37 -0400

    Refactored the substitution stuff to use a file

--- substitution.rb
+++ substitution.rb
@@ -2,28 +2,44 @@ require 'pry'
 
 class String
 
-  def encrypt(algorithm = :substitution)
+  def encrypt(algorithm = :ceasar)
     self.send("encrypt_#{algorithm}")
   end
 
-  def decrypt(algorithm = :substitution)
-    self.send("decrypt_#{algorithm}")
+  def encrypt_ceasar
+    cipher = alphabet.rotate(rand(24) + 1)
+    substitution = Hash[alphabet.zip(cipher)]
+    self.map_to(substitution)
   end
 
   def encrypt_substitution
-    alphabet = ('a'..'z').to_a
     cipher = alphabet.shuffle
     substitution = Hash[alphabet.zip(cipher)]
+    self.map_to(substitution)
+  end
 
-    self.chars.to_a.uniq.each do |c|
-      self.gsub!(c, substitution[c] || c)
+  def map_to(map)
+    length.times do |i|
+      self[i] = map[self[i]] || self[i]
     end
-
     self
+  end
+
+  def alphabet
+    @alphabet ||= ('a'..'z').to_a
   end
 
 end
 
-puts "Ruby String Encryptor - Please enter your message"
-message = gets.strip.chomp.downcase
-puts message.encrypt
+if ARGV.length == 2
+  begin
+    output_file = "encrypted.txt"
+    puts "Encrypted #{ARGV[1]} using the #{ARGV[0]} cipher to file #{output_file}"
+    message = File.open(ARGV[1]).read.strip.chomp.downcase
+    File.open(output_file, "w") { |f| f.write message.encrypt(ARGV[0]) }
+  rescue
+    puts "Could not open #{ARGV[0]}"
+  end
+else
+  puts "Usage: ruby substitution.rb <cipher: ceasar|substitution> <message_file.txt>"
+end
